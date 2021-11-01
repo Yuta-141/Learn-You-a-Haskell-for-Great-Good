43e9efb5ba62ad4c842a7b26aab3fffcf0accfce
Yuta-141 <yuta.endofeternity@gmail.com>
2021-11-01 22:49:08 +0900

    done Table of contents

--- chapter02/baby.hs
+++ chapter02/baby.hs
@@ -1,4 +1,4 @@
-doubleMe x = x + x
+doubleMe x = x + x 
 doubleUs x y = doubleMe x + doubleMe y
 
 doubleSmallNumber x = if x > 100
@@ -11,3 +11,17 @@ conanO'Brien = "It's a-me, Conan O'Brien!"
 
 
 factrial n = if n == 0 then 1 else  n * (factrial (n-1))
+
+
+boomBangs xs = [if x < 10 then "Boom" else "BANG!" | x <- xs , odd x]
+
+
+
+removeNonUppercase st = [ c | c <- st , c `elem` ['A' .. 'Z']]
+
+
+--let xxs = [[1,3,5,2,3,1,2,4,5],[1,2,3,4,5,6,7,8,9],[1,2,4,2,1,6,3,1,3,2,3,6]] 
+
+rightTriangles = [ (a,b,c) | c <-[1..10], b <- [1..c],a<- [1..b],a <- [1..b],a^2+ b^2 == c^2]
+
+rightTriangles' = [ (a,b,c) | c <-[1..10], b <- [1..c],a<- [1..b],a <- [1..b],a^2+ b^2 == c^2,a + b  + c == 24]
