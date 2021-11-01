ad6f92047e1d9fdd9a2e86f032d7840bcbe804df
Yuta-141 <yuta.endofeternity@gmail.com>
2021-11-01 11:13:09 +0900

    done chapter02

--- chapter02/baby.hs
+++ chapter02/baby.hs
@@ -8,3 +8,6 @@ doubleSmallNumber x = if x > 100
 doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
 
 conanO'Brien = "It's a-me, Conan O'Brien!"
+
+
+factrial n = if n == 0 then 1 else  n * (factrial (n-1))
